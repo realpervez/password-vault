0af621613f89c735690caad1f6a1fd493262617d
realpervez <syedpervez06@gmail.com>
2026-05-27 10:45:25 +0530

    Add UserRepository with finder and existence check methods

---
+++ src/main/java/com/pervez/password_vault/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pervez.password_vault.repository;
+import com.pervez.password_vault.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
